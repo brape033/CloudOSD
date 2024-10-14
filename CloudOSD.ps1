858790aa6aa6ce8edcdc1959b47425f180ad5e31
brape033 <62325902+brape033@users.noreply.github.com>
2024-10-14 07:26:44 +0200

    some update

--- CloudOSD.ps1
+++ CloudOSD.ps1
@@ -7,17 +7,21 @@ if ((Get-MyComputerModel) -match 'Virtual') {
     Set-DisRes 1600
 }
 
-#Make sure I have the latest OSD Content
-#Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
-#Install-Module OSD -Force
-
 Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
 Import-Module OSD -Force
 
-#TODO: Spend the time to write a function to do this and put it here
-#Write-Host  -ForegroundColor Cyan "Ejecting ISO"
-#Write-Warning "That didn't work because I haven't coded it yet!"
-#Start-Sleep -Seconds 5
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$true
+    RecoveryPartition = [bool]$true
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$false
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$false
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$false
+    CheckSHA1 = [bool]$true
+}
+
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
